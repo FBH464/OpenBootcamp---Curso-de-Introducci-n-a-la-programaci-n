c8776adb0ac0b4fc87879203e791ccc97b27315d
Francis Benavides <francis.bh.4264@gmail.com>
2022-11-20 12:05:25 -0500

    Segunda parte

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,27 @@ public class Main {
         int b = 40;
         int c = 16;
         sumaNumerosDeTresValores(a,b,c);
+
+        //Segunda parte
+        int puerta = 4;
+        System.out.println("Coche tiene 4 puertas");
+        Coche.numeroPuertas();
+        System.out.println(puerta);
+
     }
     public static int sumaNumerosDeTresValores (int a, int b, int c){
         int suma;
         suma = a + b + c;
         System.out.println(suma);
         return suma;
+    }
+    //segunda parte Coche
+    public class Coche {
+        int resultado;
+
+        public static int numeroPuertas (int resultado, int puerta){
+            resultado = puerta + 1;
+            return resultado;
+        }
     }
 }
